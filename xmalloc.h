1fd819bacc635d7322664073d300dd4b6851ddfd
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-01-10 15:03:07 -0800

    Correct header guard.

--- xmalloc.h
+++ xmalloc.h
@@ -1,5 +1,5 @@
-#ifndef VIMOL_XSTR_H
-#define VIMOL_XSTR_H
+#ifndef VIMOL_XMALLOC_H
+#define VIMOL_XMALLOC_H
 
 void *xcalloc(size_t, size_t);
 char *xstrcpy(char *, const char *);
@@ -7,4 +7,4 @@ char *xstrcat(char *, const char *);
 char *xstrdup(const char *);
 char *xstrndup(const char *, size_t);
 
-#endif /* VIMOL_XSTR_H */
+#endif /* VIMOL_XMALLOC_H */
