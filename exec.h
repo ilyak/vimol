72b25589fcacb198ca48f20eae9a93b80c293afb
Ilya Kaliman <ilya.kaliman@gmail.com>
2017-01-10 16:16:15 -0800

    Move declarations from seperate header files to vimol.h.

--- exec.h
+++
@@ -1,9 +0,0 @@
-#ifndef VIMOL_EXEC_H
-#define VIMOL_EXEC_H
-
-void exec_init(void);
-void exec_free(void);
-int exec_valid(const char *);
-int exec_run(const char *, struct tokq *, struct state *);
-
-#endif /* VIMOL_EXEC_H */
--- vimol.h
+++ vimol.h
@@ -34,13 +34,8 @@
 #include "tok.h"
 #include "sel.h"
 #include "sys.h"
-#include "edit.h"
-#include "error.h"
 #include "state.h"
-#include "cmd.h"
-#include "exec.h"
 #include "graph.h"
-#include "history.h"
 #include "log.h"
 #include "pair.h"
 #include "platform.h"
@@ -57,6 +52,10 @@
 struct alias;   /* alias management */
 struct atoms;   /* atom storage */
 struct camera;  /* an eye of a user */
+struct cmd;     /* vimol command */
+struct cmdq;    /* command list */
+struct edit;    /* string edit control */
+struct history; /* command-line history management */
 struct yank;    /* copy-paste buffer */
 
 /* alias.c */
@@ -93,6 +92,54 @@ void camera_set_radius(struct camera *, double);
 double camera_get_radius(struct camera *);
 double camera_get_zoom(struct camera *, int, int);
 point_t camera_transform(struct camera *, vec_t);
+
+/* cmd.c */
+const char *cmd_name(struct cmd *);
+struct tokq *cmd_args(struct cmd *);
+int cmd_exec(struct cmd *, struct state *);
+struct cmdq *cmdq_from_string(const char *, struct alias *);
+void cmdq_free(struct cmdq *);
+int cmdq_count(struct cmdq *);
+struct cmd *cmdq_cmd(struct cmdq *, int);
+int cmdq_exec(struct cmdq *, struct state *);
+
+/* edit.c */
+struct edit *edit_create(void);
+void edit_free(struct edit *);
+const char *edit_get_text(struct edit *);
+int edit_get_text_length(struct edit *);
+int edit_get_pos(struct edit *);
+void edit_set_pos(struct edit *, int);
+void edit_set_text(struct edit *, const char *, ...);
+void edit_insert_char(struct edit *, char);
+void edit_insert_text(struct edit *, const char *);
+void edit_backspace_char(struct edit *);
+void edit_delete_char(struct edit *);
+void edit_backspace_word(struct edit *);
+void edit_clear(struct edit *);
+
+/* error.c */
+void error_set(const char *, ...);
+const char *error_get(void);
+void error_clear(void);
+
+/* exec.c */
+void exec_init(void);
+void exec_free(void);
+int exec_valid(const char *);
+int exec_run(const char *, struct tokq *, struct state *);
+
+/* history.c */
+struct history *history_create(void);
+void history_free(struct history *);
+void history_load(struct history *, const char *);
+void history_save(struct history *, const char *);
+void history_push(struct history *, const char *);
+void history_reset_current(struct history *);
+int history_next(struct history *);
+int history_prev(struct history *);
+const char *history_get(struct history *);
+int history_search(struct history *, const char *);
 
 /* yank.c */
 struct yank *yank_create(void);
